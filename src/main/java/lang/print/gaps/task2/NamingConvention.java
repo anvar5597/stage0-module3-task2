d10fa44779b3f4d685da829a5a654fbe50e03a97
Anvar <104772445+anvar5597@users.noreply.github.com>
2022-08-27 11:30:36 +0500

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -6,8 +6,8 @@ public class NamingConvention {
         int variableOne = 1;
         int variableTwo = 2;
         int variableThree = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEFIVE = 5;
+        int variableFour = 4;
+        int variableFive = 5;
         int variableSix = 6;
         int variableseven = 7;
         int variableEight = 8;
